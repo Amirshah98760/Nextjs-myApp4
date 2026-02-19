5c82ad44681ef31ea1ff9d338e0e9a073d234ddc
Amirshah98760 <amirshah98760@gmail.com>
2026-02-19 10:42:47 +0500

    Added some images using image component and learn how to use fonts in next js

--- app/page.js
+++ app/page.js
@@ -1,21 +1,61 @@
 import Image from "next/image";
+import download from "../public/download.avif";
+import download2 from "../public/download2.avif";
 
 
 export default function Home() {
   return (
-  <div className="flex h-screen w-full mt-10 justify-center">
+ <section >
+     <div className="flex h-screen w-full mt-10 justify-evenly">
      <div className="w-70 h-70 relative  border border-gray-300 rounded-lg p-4">
    
 
     <Image
-      src="/download.avif"
+      src={download}
       alt="file Logo"
       // width={500}
       // height={500}
       fill= {true}
-      className="w-full h-full   object-cover absolute "
+      // quality={100} 
+      placeholder="blur"
+      blurDataURL=""
+      className="w-full h-full   object-cover absolute  hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
     />
      </div>
+     <div className="w-70 h-70 relative  border border-gray-300 rounded-lg p-4">
+   
+
+    <Image
+      src={download2}
+      alt="file Logo"
+      // width={500}
+      // height={500}
+      fill= {true}
+      // quality={100} 
+      placeholder="blur"
+      blurDataURL=""
+      className="w-full h-full   object-cover absolute hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
+    />
+     </div>
+     <div className="w-70 h-70 relative  border border-gray-300 rounded-lg p-4">
+   
+
+    <Image
+      src={download}
+      alt="file Logo"
+      // width={500}
+      // height={500}
+      fill= {true}
+      // quality={100} 
+      placeholder="blur"
+      blurDataURL=""
+      className="w-full h-full   object-cover absolute hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
+    />
+     </div>
+
+
   </div>
+     <h1 className="text-3xl font-bold text-center mb-20">Welcome to My App</h1>
+  </section>
   );
 }
