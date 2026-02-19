5d4b1299447e0a7ded9000e4f6a77d7ee524c91d
Amirshah98760 <amirshah98760@gmail.com>
2026-02-19 12:51:17 +0500

    I understand the client and server component and create a todo app using client component

---
+++ app/counterApp/page.jsx
@@ -0,0 +1,24 @@
+"use client"
+import React, { useState } from 'react'
+
+const counterApp = () => {
+    const [count , setCount]  = useState(0);
+if (count < 0) {
+  setCount(0);
+}
+  return (
+    <div className='flex justify-center items-center  h-screen p'>
+    <div className=' font-roboto border-2 border-gray-600 p-10 rounded-lg' >
+          <h1 className="text-3xl font-bold text-center ">Counter App</h1>
+        <p className="text-center text-2xl mt-5">{count}</p>
+        <div className="gap-4 mt-5 flex ">
+          <button onClick={() => setCount(count + 1)} className='cursor-pointer border border-gray-300 p-2 rounded '>increment</button>
+        <button onClick={() => setCount(count -1)} className='cursor-pointer border border-gray-300 p-2 rounded '>Degrement</button>
+        <button onClick={() => setCount(0)} className='cursor-pointer border border-gray-300 p-2 rounded '>Reset</button>
+        </div>  
+    </div>
+    </div>
+  )
+}
+
+export default counterApp
